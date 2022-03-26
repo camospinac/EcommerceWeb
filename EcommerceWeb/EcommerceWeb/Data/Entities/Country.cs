4757564834bc0809ce0a311776d77c8567f1d061
Camilo Ospina <camospinac@outlook.com>
2022-03-25 22:59:34 -0500

    Work finished: Controller: Countries and views and fucntional CRUD with Wizard Migrations with entity frame and explained of DataContext

---
+++ EcommerceWeb/EcommerceWeb/Data/Entities/Country.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EcommerceWeb.Data.Entities
+{
+    public class Country
+    {
+        public int Id { get; set; }
+        [Display(Name = "País")]
+        [MaxLength(50, ErrorMessage = "El campo {0} debe tener máximo {1} carácteres")]
+        [Required(ErrorMessage = "El campo {0} es obligaotorio")]
+        public string Name { get; set; }
+    }
+}
